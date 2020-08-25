58ea16e7a9c4d7824315cdd7d7a5379c72bd65b9
Diego Ferreira <diegosf1705@gmail.com>
2020-08-24 22:48:37 -0300

    Repo page created

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,9 +1,52 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { 
+  Container,
+  Breadcrumb,
+  RepoIcon,
+  Stats,
+  StarIcon,
+  ForkIcon,
+  LinkButton,
+  GithubIcon,
+} from './styles';
+import { Link } from 'react-router-dom';
 
 const Repo: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Breadcrumb>
+        <RepoIcon />
+        <Link to={`/diegods-ferreira`} className="username">
+          diegods-ferreira
+        </Link>
+        <span>/</span>
+        <Link to={`/diegods-ferreira/be-the-hero`} className="reponame">
+          be-the-hero
+        </Link>
+      </Breadcrumb>
+
+      <p>Um repositório qualquer.</p>
+
+      <Stats>
+        <li>
+          <StarIcon />
+          <b>9</b>
+          <span>stars</span>
+        </li>
+        <li>
+          <ForkIcon />
+          <b>9</b>
+          <span>forks</span>
+        </li>
+      </Stats>
+
+      <LinkButton href={`https://github.com/diegods-ferreira/be-the-hero`}>
+        <GithubIcon />
+        <span>View on Github</span>
+      </LinkButton>
+    </Container>
+  );
 }
 
 export default Repo;
