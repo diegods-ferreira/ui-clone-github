7dec03395f217f2517a2e364861aa8815cdf2052
Diego Ferreira <diegosf1705@gmail.com>
2020-08-25 09:37:13 -0300

    Header search implemented

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Container, GithubLogo, SearchForm } from './styles';
+import { useNavigate } from 'react-router-dom';
 
 const Header: React.FC = () => {
+  const [search, setSearch] = useState('');
+  const navigate = useNavigate();
+
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+
+    navigate(`/${search.toLowerCase().trim()}`);
+  }
+
   return (
     <Container>
       <GithubLogo />
-      <SearchForm>
-        <input placeholder="Enter username or repo..." />
+      <SearchForm onSubmit={handleSubmit}>
+        <input
+          placeholder="Enter username or repo..."
+          value={search}
+          onChange={event => setSearch(event.currentTarget.value)}
+        />
       </SearchForm>
     </Container>
   );
