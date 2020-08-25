07962c240465a187f0f0ebadf4bb670ea1a73c0a
Diego Ferreira <diegosf1705@gmail.com>
2020-08-24 21:03:52 -0300

    Dividing Main in left/right side with ProfileData

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Main, LeftSide, RightSide } from './styles';
+
+import ProfileData from '../../components/ProfileData';
 
 const Profile: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Main>
+        <LeftSide>
+          <ProfileData
+            username="diegods-ferreira"
+            name="Diego Ferreira"
+            avatarUrl="https://avatars1.githubusercontent.com/u/62507430?v=4"
+            followers={887}
+            following={7}
+            company="Rocketseat"
+            location="São Paulo, Brazil"
+            email="diegosf1705@gmail.com"
+            blog="https://www.linkedin.com/in/diego-de-souza-ferreira/"
+          />
+        </LeftSide>
+
+        <RightSide>
+
+        </RightSide>
+      </Main>
+    </Container>
+  );
 }
 
 export default Profile;
